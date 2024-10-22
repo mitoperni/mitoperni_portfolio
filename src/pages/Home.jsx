aba6ed85f9cf88c495d19addfcd8b31b69728723
Miguel Toyas Pernichi <mitoperni@gmail.com>
2024-10-22 13:55:52 +0100

    feat: ParallaxBackground

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import Hero from "../components/Hero/Hero";
 import About from "../components/About/About";
 import Experience from "../components/Experience/Experience";
@@ -6,28 +6,35 @@ import Projects from "../components/Projects/Projects";
 import Skills from "../components/Skills/Skills";
 import Contact from "../components/Contact/Contact";
 import "../styles/Home.css";
+import ParallaxBackground from "../components/Utils/ParallaxBackground";
 
 export default function Home() {
   return (
-    <div id="home">
-      <div id="home-hero">
-        <Hero />
+    <>
+      <ParallaxBackground />
+      <div id="home">
+        <div id="hero-and-about">
+          <div id="home-hero">
+            <Hero />
+          </div>
+          <div id="home-about">
+            <About />
+          </div>
+        </div>
+
+        <div id="home-experience">
+          <Experience />
+        </div>
+        <div id="home-projects">
+          <Projects />
+        </div>
+        <div id="home-skills">
+          <Skills />
+        </div>
+        <div id="home-contact">
+          <Contact />
+        </div>
       </div>
-      <div id="home-about">
-        <About />
-      </div>
-      <div id="home-experience">
-        <Experience />
-      </div>
-      <div id="home-projects">
-        <Projects />
-      </div>
-      <div id="home-skills">
-        <Skills />
-      </div>
-      <div id="home-contact">
-        <Contact />
-      </div>
-    </div>
+    </>
   );
 }
