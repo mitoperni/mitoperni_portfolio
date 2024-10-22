796abbe58590688beb5fda17a6e915dc132a1726
Miguel Toyas Pernichi <mitoperni@gmail.com>
2024-10-22 17:58:16 +0100

    feat: ParallaxBackground

--- src/components/Utils/ParallaxBackground.jsx
+++ src/components/Utils/ParallaxBackground.jsx
@@ -1,29 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import './ParallaxBackground.css';
 
 const ParallaxBackground = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-  
-  useEffect(() => {
-    const handleScroll = () => {
-      const position = window.scrollY;
-      setScrollPosition(position);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
   return (
-    <div className="fixed inset-0 -z-10">
-      <div 
-        className="absolute inset-0 bg-cover bg-center transition-opacity duration-1000"
-        style={{
-          backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg')`,
-          opacity: scrollPosition < window.innerHeight * 1.5 ? 1 - (scrollPosition / (window.innerHeight * 1.5)) : 0,
-          transform: `translateY(${scrollPosition * 0.5}px)`
-        }}
-      />
-      <div className="absolute inset-0 bg-black" />
+    <div className="parallax-container">
+      <div className="parallax-background"></div>
+      <div className="parallax-gradient-overlay"></div>
     </div>
   );
 };
